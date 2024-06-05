79c4db49b502e557dbe45d99fd4d741ff220b432
choudhrySohail <chshoail19971@gmail.com>
2024-06-05 17:05:28 +0500

    first commit

---
+++ ex_06.ts
@@ -0,0 +1,19 @@
+/*
+Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+*/
+
+//const myName: string = "  Choudhry sohail  ";
+
+//const myName: string = "       Choudhry sohail     "
+//console.log(myName);
+
+// \t used for tab/space
+//const myName: string = "\tChoudhry sohail"
+//console.log(myName);
+
+// \n used for new line
+const myName: string = "\nChoudhry sohail"
+//console.log(myName);
+
+//console.log(myName.trim());
+console.log(myName.trim());
